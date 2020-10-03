5e43dae54a9715737b3f5be9202b68f5566b0ede
Jeremy Telhiard <jeremy.telhiard@gmail.com>
2020-10-02 23:00:21 -0500

    Starting round implemented

--- gameJS.js
+++ gameJS.js
@@ -1,7 +1,20 @@
 const holes = document.getElementsByClassName('hole');
 const score = document.getElementsByClassName('score');
 const moles = document.getElementsByClassName('mole');
+let startButton = document.getElementsByClassName('start');
+
 let lastHole;
+let timeUp = false;
+
+startButton.onclick = start();
+
+function start() {
+    score.textContent = 0;
+    timeUp = false;
+    randomizeMole();
+    setTimeout(() =>
+    timeUp = true, 20000);
+}
 
 function randomizeTime(min, max) {
     return Math.round(Math.random() * (max - min) + min);
@@ -15,4 +28,16 @@ function randomizeHole(holes) {
     }
     lastHole = hole;
     return hole;
+}
+
+function randomizeMole() {
+    const time = randomizeTime(200, 1000);
+    const hole = randomizeHole(holes);
+    hole.classList.add('up');
+    setTimeout(() => {
+        hole.classList.remove('up');
+        if(!timeUp){
+            randomizeMole();
+        }
+    }, time);
 }
